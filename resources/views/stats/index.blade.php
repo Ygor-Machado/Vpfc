f619a501a59e06804367fa4e84f7e5b100f525ce
Ygor-Machado <ygaounderground011@gmail.com>
2024-08-24 00:52:34 -0300

    feat: cria index das estatisticas dos jogadores

--- resources/views/stats/index.blade.php
+++ resources/views/stats/index.blade.php
@@ -1,7 +1,61 @@
-@extends('layouts.dashboard.main')
+<x-app-layout>
+    <x-slot name="header">
+        <h1>Estastícas</h1>
+    </x-slot>
 
-@section('title', 'Estatistica')
 
-@section('content')
 
-@endsection
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Nome do Jogador
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Gols
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Assistências
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Cartão Amarelo
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Cartão Vermelho
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    <span class="sr-only">Edit</span>
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+                @foreach($stats as $stat)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="px-6 py-4">
+                            {{ $stat->player->name }}
+                        </td>
+
+                        <td class="px-6 py-4">
+                            {{ $stat->goals }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $stat->assists }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $stat->yellow_cards }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $stat->red_cards }}
+                        </td>
+
+                        <td class="px-6 py-4 text-right">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     route::resource('/players', PlayerController::class);
     Route::resource('/departures', DepartureController::class);
-    route::resource('/playerstats', PlayerStatController::class);
+    route::resource('/stats', PlayerStatController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
